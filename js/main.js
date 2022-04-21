56fe039927cfb95dab4297e1ec2e0febd7a35ee2
+ <+>
2022-04-21 17:00:30 +0300

    Styling the hidden movie-result display div

--- js/main.js
+++ js/main.js
@@ -98,3 +98,22 @@ function loadMovieDetails(){
     });
 }
 
+function displayMovieDetails(details){
+    resultGrid.innerHTML = `
+    <div class = "movie-poster">
+        <img src = "${(details.Poster != "N/A") ? details.Poster : "image_not_found.png"}" alt = "movie poster">
+    </div>
+    <div class = "movie-info">
+        <h3 class = "movie-title">${details.Title}</h3>
+        <p class = "writer"><b>Writer:</b> ${details.Writer}</p>
+        <p class = "actors"><b>Actors: </b>${details.Actors}</p>
+        <p class = "plot"><b>Plot:</b> ${details.Plot}</p>
+        <p class = "genre"><b>Genre:</b> ${details.Genre}</p>
+
+        <ul class = "movie-misc-info">
+            <li class = "released">Released: ${details.Released}</li>
+        </ul>
+    </div>
+    `;
+}
+
